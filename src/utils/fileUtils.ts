c50cc3f6ebd6957d49f17957ed39ede22b1e7002
Indrajithinna <indrajithinna@gmail.com>
2026-01-26 20:35:43 +0530

    fix(utils): support PB in formatFileSize

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -8,7 +8,7 @@
 export const formatFileSize = (bytes: number): string => {
     if (bytes === 0) return '0 Bytes';
     const k = 1024;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return Math.round((bytes / Math.pow(k, i)) * 100) / 100 + ' ' + sizes[i];
 };
