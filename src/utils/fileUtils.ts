2676211de905c214d27cda00eb808294fa0ee1f0
Indrajithinna <indrajithinna@gmail.com>
2026-02-03 13:53:41 +0530

    Refactor: Add debounce utility to fileUtils

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -128,3 +128,14 @@ export const getFileExtension = (filename: string): string => {
     return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
 };
 
+/**
+ * Debounce function to limit rate of execution
+ */
+export const debounce = <T extends (...args: any[]) => void>(func: T, wait: number): ((...args: Parameters<T>) => void) => {
+    let timeout: ReturnType<typeof setTimeout>;
+    return (...args: Parameters<T>) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func(...args), wait);
+    };
+};
+
